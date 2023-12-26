f6526956c7e5833c905210da3a20300534884488
cyk <1076504979@qq.com>
2023-12-26 10:02:19 +0100

    update requirements

---
+++ init.sh
@@ -0,0 +1,3 @@
+pip install numpy Pillow scikit-learn scipy tqdm wget open3d tifffile scikit-image kornia opencv-python loguru seaborn
+pip install timm==0.6.11
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
